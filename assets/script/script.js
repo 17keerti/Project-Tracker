c7e2a8604405f5a1645db390ec9cff587fb9f68f
atmason90 <atmason90@gmail.com>
2022-04-06 12:38:55 -0700

    start on code

---
+++ assets/script/script.js
@@ -0,0 +1,12 @@
+// capture form data with JQuery and create new row on the page
+var formInputsEl = $("#userForm");
+var projectNameEl = $("input[name='projectName']");
+var projectTypeEl = $("input[name='projectType']");
+var hourlyWageEl = $("input[name='hourlyWage']");
+var dueDateEl = $("input[name='dueDate']");
+
+function handleFormSubmit (event) {
+    event.preventDefault();
+
+    
+}
